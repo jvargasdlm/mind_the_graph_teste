850e4cbe467462d67ae6a285a4290478bf642851
Javier Vargas de la Maza <javiervargasdlm@gmail.com>
2022-06-07 19:52:55 -0300

    logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
 import Main from './Main';
 import Conteudo from './Conteudo';
 
 function App() {
   return (
     <div className="App">
-      <Main />
-      <Conteudo />
+      <Header />
+      {/* <Main />
+      <Conteudo /> */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <div className='top'>
+            <div className="header-options">
+                <div className="logo_mindthegraph"></div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
