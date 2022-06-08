7d934bd942eb43f10cd4b77f156e3413cfb3e136
Javier Vargas de la Maza <javiervargasdlm@gmail.com>
2022-06-08 01:48:06 -0300

    responsividade header

--- src/Header.js
+++ src/Header.js
@@ -8,8 +8,12 @@ function Header(){
                     <a href="https://mindthegraph.com/app/pricing" className="header-links">Pricing</a>
                     <a href="https://mindthegraph.com/blog" className="header-links">Blog</a>
                     <a href="https://mindthegraph.com/app/custom-services" className="header-links">Jobs</a>
-                    <a href="https://mindthegraph.com/app/custom-services" className="header-login">Login</a>
-                    <a href="https://mindthegraph.com/app/custom-services" className="header-signup">Signupfree</a>
+                    <a href="https://mindthegraph.com/app/auth/login?from_url=/" className="header-login">
+                        <span>Login</span>
+                        </a>
+                    <a href="https://mindthegraph.com/app/auth/register?from_url=/app/custom-services" className="header-signup">
+                        <span>Sign up free</span>
+                    </a>
                 </div>
             </div>
         </div>
