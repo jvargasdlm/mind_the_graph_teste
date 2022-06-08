f0113dff3caf50a18c4d1ca3f5be94bd0efe52e8
Javier Vargas de la Maza <javiervargasdlm@gmail.com>
2022-06-07 23:40:13 -0300

    ajustes header

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,12 @@ function Header(){
             <div className="header-options">
                 <div className="logo_mindthegraph"></div>
                 <div className="buttons-container">
-                    <a href="https://mindthegraph.com/app/infographic-templates" className="header-button-1">Templates</a>
-                    <a href="https://mindthegraph.com/app/pricing" className="header-button-1">Pricing</a>
-                    <a href="https://mindthegraph.com/blog" className="header-button-1">Blog</a>
-                    <a href="https://mindthegraph.com/app/custom-services" className="header-button-1">Jobs</a>
+                    <a href="https://mindthegraph.com/app/infographic-templates" className="header-links">Templates</a>
+                    <a href="https://mindthegraph.com/app/pricing" className="header-links">Pricing</a>
+                    <a href="https://mindthegraph.com/blog" className="header-links">Blog</a>
+                    <a href="https://mindthegraph.com/app/custom-services" className="header-links">Jobs</a>
+                    <a href="https://mindthegraph.com/app/custom-services" className="header-login">Login</a>
+                    <a href="https://mindthegraph.com/app/custom-services" className="header-signup">Signupfree</a>
                 </div>
             </div>
         </div>
