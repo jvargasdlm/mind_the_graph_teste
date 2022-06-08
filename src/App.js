187a04db00b87d391b2fbedf407e153870cd15c9
Javier Vargas de la Maza <javiervargasdlm@gmail.com>
2022-06-07 22:39:14 -0300

    add botões

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Main />
-      <Conteudo /> */}
+      <Main />
+      <Conteudo />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,14 @@
 function Header(){
     return(
-        <div className='top'>
+        <div className='header'>
             <div className="header-options">
                 <div className="logo_mindthegraph"></div>
+                <div className="buttons-container">
+                    <a href="https://mindthegraph.com/app/infographic-templates" className="header-button-1">Templates</a>
+                    <a href="https://mindthegraph.com/app/pricing" className="header-button-1">Pricing</a>
+                    <a href="https://mindthegraph.com/blog" className="header-button-1">Blog</a>
+                    <a href="https://mindthegraph.com/app/custom-services" className="header-button-1">Jobs</a>
+                </div>
             </div>
         </div>
     )
