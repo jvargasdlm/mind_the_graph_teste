786b6ad145ae8268a976098a39c55beedee9a4e7
Javier Vargas de la Maza <javiervargasdlm@gmail.com>
2022-06-08 00:00:48 -0300

    deleção arquivos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header'
-import Main from './Main';
-import Conteudo from './Conteudo';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
-      <Conteudo />
     </div>
   );
 }
